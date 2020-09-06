ff78b8335d14502feedcb7016392e26ef790e00f
Riyad0821 <mdriyad0821@gmail.com>
2020-09-06 23:48:38 +0600

    creating components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Post from './Components/Post/Post';
 
 function App() {
+
   return (
     <div>
-      
+      <Post></Post>
     </div>
   );
 }
